270e3d38ae6acf126c45df7dd144d2ff05111837
David Kassin <david.j.kassin@gmail.com>
2022-07-13 07:42:23 -0600

    Add functionality for saved, makecover and home buttons

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ var taglineOne = document.querySelector('.tagline-1');
 var taglineTwo = document.querySelector('.tagline-2');
 
 
+var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view');
+var savedCoversSection = document.querySelector('.saved-covers-section');
+var formView = document.querySelector('.form-view')
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -20,6 +25,9 @@ var currentCover;
 // Add your event listeners here 👇
 // window.addEventListner('load', newRandomCover);
 randomCoverButton.addEventListener('click', newRandomCover);
+viewSavedButton.addEventListener('click', displaySaved);
+makeNewButton.addEventListener('click', displayForm);
+homeButton.addEventListener('click', displayHome);
 // Create your event handlers and other functions here 👇
 
 function newRandomCover() {
@@ -36,6 +44,41 @@ function displayCover() {
   coverTitle.innerText = currentCover.title;
   taglineOne.innerText = currentCover.tagline1;
   taglineTwo.innerText = currentCover.tagline2;
+}
+
+function show(element) {
+  element.classList.remove('hidden')
+}
+
+function hide(element) {
+  element.classList.add('hidden')
+}
+
+function displayForm() {
+  show(formView)
+	show(homeButton)
+	hide(homeView)
+	hide(savedView)
+	hide(saveCoverButton)
+	hide(randomCoverButton)
+}
+
+function displaySaved() {
+  show(savedView)
+  show(homeButton)
+  hide(homeView)
+  hide(formView)
+  hide(saveCoverButton)
+  hide(randomCoverButton)
+}
+
+function displayHome() {
+  show(homeView)
+  show(saveCoverButton)
+  show(randomCoverButton)
+  show(viewSavedButton)
+  show(makeNewButton)
+  hide(homeButton)
 }
 
 // We've provided one function to get you started
